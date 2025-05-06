de9736e57e2a174682f8a59e273a89f802a1987a
manish <aryam7842@gmail.com>
2025-05-06 16:08:25 +0530

    Implimented hot reloding using Tailwind

--- E_COMM/settings.py
+++ E_COMM/settings.py
@@ -40,7 +40,14 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme' # This is the name of the app that will be used to generate the tailwind files
+INTERNAL_IPS = ['127.0.0.1']
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,9 +57,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'E_COMM.urls'
+
 
 TEMPLATES = [
     {
