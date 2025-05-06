1ca76c2f0878966eb22442d9f596540601275928
manish <aryam7842@gmail.com>
2025-05-06 17:47:00 +0530

    Fix path issue

--- category/models.py
+++ category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Category(models.Model):
     Category_name = models.CharField(max_length=50,unique=True)
     slug_field=models.SlugField(max_length=50,unique=True)
@@ -9,5 +9,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = ('category')
         verbose_name_plural = ('categories')
+        
+    def get_url(self):
+        return reverse('product_by_category', args=[self.slug_field])
+            
     def __str__(self):
         return self.Category_name
