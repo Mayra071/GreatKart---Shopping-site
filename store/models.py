2131e2ea6890de6563b09812bcccb71333a40f6a
manish <aryam7842@gmail.com>
2025-05-12 12:30:14 +0530

    Added product detail page

--- store/models.py
+++ store/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 from django.urls import reverse
 from category.models import Category
+from django.core.exceptions import ValidationError
+
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
-    slug_field=models.SlugField(max_length=200, unique=True)
+    slug_field = models.SlugField(max_length=200, unique=True)
     description = models.TextField(blank=True, max_length=500)
-    price=models.IntegerField()
-    image=models.ImageField(upload_to='images/product')
-    stock=models.IntegerField()
-    is_available=models.BooleanField(default=True)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='images/product')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    modified_date=models.DateTimeField(auto_now=True)
+    modified_date = models.DateTimeField(auto_now=True)
     
     class Meta:
-        verbose_name=('product')
-        verbose_name_plural=('products')
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+        
+    def clean(self):
+        if self.category and not Category.objects.filter(id=self.category.id).exists():
+            raise ValidationError({'category': 'Selected category does not exist.'})
+        
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
         
     def get_absolute_url(self):
-        return reverse('product_detail', args=[self.category.slug_field, self.slug_field])
+        if self.category:
+            return reverse('product_detail', args=[self.category.slug_field, self.slug_field])
+        return reverse('store')
         
     def __str__(self):
         return self.product_name
--- store/views.py
+++ store/views.py
@@ -26,5 +26,16 @@ def store(request, category_slug=None):
 
 
 def product_detail(request, category_slug, product_slug):
-    return render(request, 'store/product_detail.html')
+    try:
+        single_product = Product.objects.get(category__slug_field=category_slug, slug_field=product_slug)
+    except Product.DoesNotExist:
+        single_product = None
+    if single_product is None:
+        return render(request, '404.html', status=404)
+    
+    context= {
+        'single_product': single_product,
+        # 'categories': Category.objects.all(),
+    }
+    return render(request, 'store/product_detail.html',context)
     
